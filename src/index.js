d850ab4f86c1f15ce1810f0f8ddd84830a6d70bf
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-08-29 22:34:19 -0400

    Added send message ability
    Can now send a message to another node through SamCore.
    
    This also enables some error handling in case a call goes south on
    SamCore.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ let ConsoleArgs = {
       .usage('[OPTIONS]...')
       .option('-n, --does-node-exist <value>', 'Find out if a node is active in the network')
       .option('-s, --does-settings-exist', 'Find out if the settings file for SamCore exists')
+      .option('-m, --send-message <message>', 'Send a message to another node')
+      .option('--node <node>', 'Specify node to send message to')
       .parse(process.argv);
       // .option('-f, --flag', 'Detects if the flag is present.')
 
@@ -29,6 +31,9 @@ function main() {
   SamCore.nodeName = 'console';
 
   SamCore.onConnect = () => {
+    // for debugging
+    // Helpers.log({loud: true}, 'Console Arguments: ', ConsoleArgs.options);
+
     if ('doesNodeExist' in ConsoleArgs.options) {
       const nodeName = ConsoleArgs.options.doesNodeExist;
       SamCore.doesNodeExist(nodeName);
@@ -37,15 +42,54 @@ function main() {
     else if ('doesSettingsExist' in ConsoleArgs.options) {
       SamCore.doesSettingsExist();
     }
+
+    else if ('sendMessage' in ConsoleArgs.options) {
+      if ('node' in ConsoleArgs.options) {
+        SamCore.sendMessage({
+          nodeSender: SamCore.nodeName,
+          nodeReceiver: ConsoleArgs.options.node,
+          apiCall: 'none',
+          packet: {message: ConsoleArgs.options.sendMessage}
+        });
+      } else {
+        Helpers.log({leader: 'warning', loud: true}, 'sendMessage action requres --node argument!');
+        SamCore.disconnect();
+      }
+    }
+  }
+
+  /**
+   * Reminders on these built-in functions to communicate with SamCore:
+   *  - You need to set up a call/response setup
+   *  - data being received by these functions only contain the data.packet info
+   *    - does not include nodeSender,nodeReceiver,apiCall
+   * 
+   * TODO: Need to actually add logic to filter out messages.
+   */
+  SamCore.onMessage = (data) => {
+    Helpers.log({leader: 'highlight', loud: true}, 'Message Response: ', data);
+    SamCore.disconnect();
+  }
+
+  SamCore.onError = (data) => {
+    Helpers.log({loud: true}, JSON.stringify({
+      response: "error",
+      error: data.response
+    }));
+    SamCore.disconnect();
   }
 
   SamCore.doesNodeExistReturn = (data) => {
-    Helpers.log({loud: true}, `{"response": ${data.response ? 1 : 0}}`);
+    Helpers.log({loud: true}, JSON.stringify({
+      response: data.response ? 1 : 0
+    }));
     SamCore.disconnect();
   }
 
   SamCore.doesSettingsExistReturn = (data) => {
-    Helpers.log({loud: true}, `{"response": ${data.response ? 1 : 0}}`);
+    Helpers.log({loud: true}, JSON.stringify({
+      response: data.response ? 1 : 0
+    }));
     SamCore.disconnect();
   }
 
