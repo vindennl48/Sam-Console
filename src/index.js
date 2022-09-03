4d7008ff035ba0e837c2c99594cf553ab26b13bc
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-09-03 19:43:32 -0400

    Got rid of main()
    I originally had the main() in there to easily shut off the entire
    program and do testing code, however, this is now unnecessary.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,47 @@
-function main() {
-  const { Client }  = require('../../SamCore/src/Client.js');
-  const { Helpers } = require('../../SamCore/src/Helpers.js');
+const { Client }  = require('../../SamCore/src/Client.js');
+const { Helpers } = require('../../SamCore/src/Helpers.js');
 
-  // Setup all of the command line arguments
-  let ConsoleArgs = {
-    commander: null,
-    options: null,
+// Setup all of the command line arguments
+let ConsoleArgs = {
+  commander: null,
+  options: null,
 
-    setup() {
-      this.commander = require('commander');
+  setup() {
+    this.commander = require('commander');
 
-      this.commander
-        .version('1.0.0', '-v, --version')
-        .usage('[OPTIONS]...')
-        .option('-n, --does-node-exist <value>', 'Find out if a node is active in the network')
-        .option('-m, --send-message <message>', 'Send a message to another node')
-        .option('--node <node>', 'Specify node to send message to')
-        .parse(process.argv);
-        // .option('-f, --flag', 'Detects if the flag is present.')
+    this.commander
+      .version('1.0.0', '-v, --version')
+      .usage('[OPTIONS]...')
+      .option('-n, --does-node-exist <value>', 'Find out if a node is active in the network')
+      .option('-m, --send-message <message>', 'Send a message to another node')
+      .option('--node <node>', 'Specify node to send message to')
+      .parse(process.argv);
+      // .option('-f, --flag', 'Detects if the flag is present.')
 
-      this.options = this.commander.opts();
-    }
+    this.options = this.commander.opts();
   }
-  ConsoleArgs.setup();
-
-  Helpers.log_silent = true;
-
-  let nodeName   = 'Console';
-  let serverName = 'SamCore';
-  let node       = new Client(nodeName, serverName);
-
-  node
-    .addApiCall('return', function(packet) {
-      if ( this.receive(serverName, 'doesNodeExist', packet) ) {
-        Helpers.log({loud: true}, packet.data);
-        this.ipc.disconnect(serverName);
-      }
-    })
-    .run(function() {
-      if ('doesNodeExist' in ConsoleArgs.options) {
-        this.call(serverName, 'doesNodeExist', ConsoleArgs.options.doesNodeExist);
-      }
-      if ('sendMessage' in ConsoleArgs.options) {
-        this.call(ConsoleArgs.options.node, 'message', ConsoleArgs.options.sendMessage);
-        this.ipc.disconnect(serverName);
-      }
-    });
-
 }
-main();
+ConsoleArgs.setup();
+
+Helpers.log_silent = true;
+
+let nodeName   = 'Console';
+let serverName = 'SamCore';
+let node       = new Client(nodeName, serverName);
+
+node
+  .addApiCall('return', function(packet) {
+    if ( this.receive(serverName, 'doesNodeExist', packet) ) {
+      Helpers.log({loud: true}, packet.data);
+      this.ipc.disconnect(serverName);
+    }
+  })
+  .run(function() {
+    if ('doesNodeExist' in ConsoleArgs.options) {
+      this.call(serverName, 'doesNodeExist', ConsoleArgs.options.doesNodeExist);
+    }
+    if ('sendMessage' in ConsoleArgs.options) {
+      this.call(ConsoleArgs.options.node, 'message', ConsoleArgs.options.sendMessage);
+      this.ipc.disconnect(serverName);
+    }
+  });
