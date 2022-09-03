d70d5c5e93634e37c30c7d94955b16d8c946a3ae
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2022-09-03 18:44:23 -0400

    Updated index.js
    This update includes the updates to the new Client.js library in
    SamCore.

--- src/index.js
+++ src/index.js
@@ -1,107 +1,51 @@
-/**
- * TODO: Add cli argument for node-ipc verbose
- */
-
-const { SamCore, Helpers } = require('./SamCore.js');
-
-let ConsoleArgs = {
-  commander: null,
-  options: null,
-
-  setup() {
-    this.commander = require('commander');
-
-    this.commander
-      .version('1.0.0', '-v, --version')
-      .usage('[OPTIONS]...')
-      .option('-n, --does-node-exist <value>', 'Find out if a node is active in the network')
-      .option('-s, --does-settings-exist', 'Find out if the settings file for SamCore exists')
-      .option('-m, --send-message <message>', 'Send a message to another node')
-      .option('--node <node>', 'Specify node to send message to')
-      .parse(process.argv);
-      // .option('-f, --flag', 'Detects if the flag is present.')
-
-    this.options = this.commander.opts();
-  }
-}
-
 function main() {
-  Helpers.log_silent = true; // Hide all logs, except ones we want
+  const { Client }  = require('../../SamCore/src/Client.js');
+  const { Helpers } = require('../../SamCore/src/Helpers.js');
 
-  // Setup outside arguments
+  // Setup all of the command line arguments
+  let ConsoleArgs = {
+    commander: null,
+    options: null,
+
+    setup() {
+      this.commander = require('commander');
+
+      this.commander
+        .version('1.0.0', '-v, --version')
+        .usage('[OPTIONS]...')
+        .option('-n, --does-node-exist <value>', 'Find out if a node is active in the network')
+        .option('-m, --send-message <message>', 'Send a message to another node')
+        .option('--node <node>', 'Specify node to send message to')
+        .parse(process.argv);
+        // .option('-f, --flag', 'Detects if the flag is present.')
+
+      this.options = this.commander.opts();
+    }
+  }
   ConsoleArgs.setup();
 
-  // Name of the node we are running
-  SamCore.nodeName = 'console';
+  Helpers.log_silent = true;
 
-  SamCore.onConnect = () => {
-    // for debugging
-    // Helpers.log({loud: true}, 'Console Arguments: ', ConsoleArgs.options);
+  let nodeName   = 'Console';
+  let serverName = 'SamCore';
+  let node       = new Client(nodeName, serverName);
 
-    if ('doesNodeExist' in ConsoleArgs.options) {
-      const nodeName = ConsoleArgs.options.doesNodeExist;
-      SamCore.doesNodeExist(nodeName);
-    }
-
-    else if ('doesSettingsExist' in ConsoleArgs.options) {
-      SamCore.doesSettingsExist();
-    }
-
-    else if ('sendMessage' in ConsoleArgs.options) {
-      if ('node' in ConsoleArgs.options) {
-        SamCore.sendMessage({
-          nodeSender: SamCore.nodeName,
-          nodeReceiver: ConsoleArgs.options.node,
-          apiCall: 'none',
-          packet: {message: ConsoleArgs.options.sendMessage}
-        });
-      } else {
-        /**
-          * TODO: change the output to json format so it can be interpreted by LUA
-          * */
-        Helpers.log({leader: 'warning', loud: true}, 'sendMessage action requres --node argument!');
-        SamCore.disconnect();
+  node
+    .addApiCall('return', function(packet) {
+      if ( this.receive(serverName, 'doesNodeExist', packet) ) {
+        Helpers.log({loud: true}, packet.data);
+        this.ipc.disconnect(serverName);
       }
-    }
-  }
+    })
+    .run(function() {
+      if ('doesNodeExist' in ConsoleArgs.options) {
+        this.call(serverName, 'doesNodeExist', ConsoleArgs.options.doesNodeExist);
+      }
+      if ('sendMessage' in ConsoleArgs.options) {
+        this.call(ConsoleArgs.options.node, 'message', ConsoleArgs.options.sendMessage);
+        this.ipc.disconnect(serverName);
+      }
+    });
 
-  /**
-   * Reminders on these built-in functions to communicate with SamCore:
-   *  - You need to set up a call/response setup
-   *  - data being received by these functions only contain the data.packet info
-   *    - does not include nodeSender,nodeReceiver,apiCall
-   * 
-   * TODO: Need to actually add logic to filter out messages.
-   */
-  SamCore.onMessage = (data) => {
-    Helpers.log({leader: 'highlight', loud: true}, 'Message Response: ', data);
-    SamCore.disconnect();
-  }
-
-  SamCore.onError = (data) => {
-    Helpers.log({loud: true}, JSON.stringify({
-      response: "errorMessage",
-      error: data.response
-    }));
-    SamCore.disconnect();
-  }
-
-  SamCore.doesNodeExistReturn = (data) => {
-    Helpers.log({loud: true}, JSON.stringify({
-      response: data.response
-    }));
-    SamCore.disconnect();
-  }
-
-  SamCore.doesSettingsExistReturn = (data) => {
-    Helpers.log({loud: true}, JSON.stringify({
-      response: data.response
-    }));
-    SamCore.disconnect();
-  }
-
-  // Start the connection process
-  SamCore.run();
 }
-
 main();
